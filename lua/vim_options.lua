ac84a57d47a23f52868426e52fed71f216206355
gameboy <mgebarski@gmail.com>
2024-04-19 16:08:38 +0200

    Set vim options

--- lua/vim_options.lua
+++ lua/vim_options.lua
@@ -1,10 +1,38 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-
 vim.g.mapleader = ","
 vim.g.background = "light"
 vim.opt.swapfile = false
 vim.wo.relativenumber = true
 vim.opt.clipboard = "unnamedplus"
+vim.opt.fileencoding = "utf-8"
+vim.opt.cursorline = true
+vim.opt.breakindent = true
+vim.opt.cmdheight = 0
+vim.opt.completeopt = {"menu", "menuone", "noselect"}
+vim.opt.copyindent = true
+vim.opt.expandtab = true
+vim.opt.fillchars = {eob = " "}
+vim.opt.foldenable = true
+vim.opt.foldlevel = 99
+vim.opt.foldlevelstart = 99
+vim.opt.history = 100
+vim.opt.ignorecase = true
+vim.opt.infercase = true
+vim.opt.laststatus = 3
+vim.opt.linebreak = true
+vim.opt.preserveindent = true
+vim.opt.shiftwidth = 2
+vim.opt.signcolumn = "auto"
+vim.opt.smartcase = true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.tabstop = 2
+vim.opt.termguicolors = true
+vim.opt.timeoutlen = 500
+vim.opt.title = true
+vim.opt.undofile = true
+vim.opt.updatetime = 300
+vim.opt.virtualedit = "block"
+vim.opt.wrap = false
+vim.opt.writebackup = false
+vim.opt.softtabstop = 2
+vim.opt.foldmethod = "manual"
