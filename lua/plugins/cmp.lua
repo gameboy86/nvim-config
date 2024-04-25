097a21be9067143dd1672ebbd7668edc79f8f373
Maciej Gębarski <mgebarski@gmail.com>
2024-04-25 23:29:33 +0200

    Change cmp run function

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -28,12 +28,12 @@ return {
 				mapping = cmp.mapping.preset.insert({
 					["<C-b>"] = cmp.mapping.scroll_docs(-4),
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					["<C-Space>"] = cmp.mapping.complete(),
+					["<Leader><Leader>"] = cmp.mapping.complete(),
 					["<C-e>"] = cmp.mapping.abort(),
 					["<CR>"] = cmp.mapping.confirm({ select = true }),
-          ["<tab>"] = cmp.mapping.select_next_item(),
-          ["<C-j>"] = cmp.mapping.select_next_item(),
-          ["<C-k>"] = cmp.mapping.select_prev_item(),
+					["<tab>"] = cmp.mapping.select_next_item(),
+					["<C-j>"] = cmp.mapping.select_next_item(),
+					["<C-k>"] = cmp.mapping.select_prev_item(),
 				}),
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
