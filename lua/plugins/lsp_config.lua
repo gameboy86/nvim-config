55b542f6cb3a26fac3f26f792eacd8d2e1b592a5
Maciej Gębarski <mgebarski@gmail.com>
2024-04-26 22:58:52 +0200

    Add ufo folding plugin

--- lua/plugins/lsp_config.lua
+++ lua/plugins/lsp_config.lua
@@ -16,24 +16,30 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
-      local tb = require("telescope.builtin")
+			local tb = require("telescope.builtin")
 			local capa = require("cmp_nvim_lsp").default_capabilities()
+			capa.textDocument.foldingRange = {
+				dynamicRegistration = false,
+				lineFoldingOnly = true,
+			}
 			local lspconfig = require("lspconfig")
 			lspconfig.lua_ls.setup({
-        capabilities = capa
-      })
+				capabilities = capa,
+			})
 			lspconfig.gopls.setup({
-        capabilities = capa
-      })
+				capabilities = capa,
+			})
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "<leader>la", vim.lsp.buf.code_action, {})
-      vim.keymap.set("n", "<leader>ld", function() vim.diagnostic.open_float() end)
-      vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, {})
-      vim.keymap.set("n", "<leader>lh", vim.lsp.buf.signature_help, {})
+			vim.keymap.set("n", "<leader>ld", function()
+				vim.diagnostic.open_float()
+			end)
+			vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, {})
+			vim.keymap.set("n", "<leader>lh", vim.lsp.buf.signature_help, {})
 			vim.keymap.set("n", "gd", tb.lsp_definitions, {})
-      vim.keymap.set("n", "<leader>lR", tb.lsp_references, {})
-      vim.keymap.set("n", "gI", tb.lsp_implementations, {})
-      vim.keymap.set("n", "gy", tb.lsp_type_definitions, {})
+			vim.keymap.set("n", "<leader>lR", tb.lsp_references, {})
+			vim.keymap.set("n", "gI", tb.lsp_implementations, {})
+			vim.keymap.set("n", "gy", tb.lsp_type_definitions, {})
 		end,
 	},
 }
