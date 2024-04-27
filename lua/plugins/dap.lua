b6fa66b7cfee442ed1a3ce6696196ea9e0aa7b76
gameboy <mgebarski@gmail.com>
2024-04-27 16:24:25 +0200

    Change dap ui display settings

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -2,17 +2,59 @@ return {
 	"mfussenegger/nvim-dap",
 	dependencies = {
 		"leoluz/nvim-dap-go",
+		"nvim-neotest/nvim-nio",
 		"rcarriga/nvim-dap-ui",
 		"theHamsta/nvim-dap-virtual-text",
 		"nvim-neotest/nvim-nio",
 	},
 	config = function()
-		require("dapui").setup()
 		require("nvim-dap-virtual-text").setup()
 		require("dap-go").setup()
-
+		require("dapui").setup({
+			icons = { expanded = "▾", collapsed = "▸", current_frame = "▸" },
+			mappings = {
+				expand = { "<CR>", "<2-LeftMouse>" },
+				open = "o",
+				remove = "d",
+				edit = "e",
+				repl = "r",
+				toggle = "t",
+			},
+			layouts = {
+				{
+					elements = {
+						{ id = "scopes", size = 0.25 },
+						"breakpoints",
+						"stacks",
+						"watches",
+					},
+					size = 0.15,
+					position = "left",
+				},
+				{
+					elements = {
+						"repl",
+						"console",
+					},
+					size = 0.25,
+					position = "bottom",
+				},
+			},
+			floating = {
+				max_height = nil,
+				max_width = nil,
+				border = "single",
+				mappings = {
+					close = { "q", "<Esc>" },
+				},
+			},
+			windows = { indent = 1 },
+			render = {
+				max_type_length = nil,
+				max_value_lines = 100,
+			},
+		})
 		local dap, dapui = require("dap"), require("dapui")
-
 		dap.listeners.before.attach.dapui_config = function()
 			dapui.open()
 		end
