60c052bc2e7595ee8782775e894bc9938973be37
gameboy <mgebarski@gmail.com>
2024-04-27 16:17:02 +0200

    Display relative path in lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -5,11 +5,20 @@ return {
 		require("lualine").setup({
 			options = {
 				theme = "auto",
-				sections = {
-					lualine_a = {
-						file = 1,
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diff", "diagnostics" },
+				lualine_c = {
+					{
+						"filename",
+						file_status = true, -- displays file status (readonly status, modified status)
+						path = 1, -- 0 = just filename, 1 = relative path, 2 = absolute path
 					},
 				},
+				lualine_x = { "encoding", "fileformat", "filetype" },
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
 			},
 		})
 	end,
