a6c93aa0a2aa5fa63a81f4b6b4390cef7a087d61
Maciej Gębarski <mgebarski@gmail.com>
2024-04-28 18:33:14 +0200

    Set colorschema

---
+++ lua/plugins/colorschema.lua
@@ -0,0 +1,43 @@
+return {
+  -- {
+  --   "neanias/everforest-nvim",
+  --   config = function()
+  --     vim.cmd.colorscheme("everforest")
+  --   end,
+  -- },
+  {
+    "EdenEast/nightfox.nvim",
+    config = function()
+      vim.cmd.colorscheme("nightfox")
+    end,
+  },
+  -- {
+  --   "catppuccin/nvim",
+  --   name = "catppuccin",
+  --   priority = 1000,
+  --   config = function()
+  --     vim.cmd.colorscheme("catppuccin")
+  --   end,
+  -- },
+  -- {
+  --   "rebelot/kanagawa.nvim",
+  --   config = function()
+  --     vim.cmd.colorscheme("kanagawa-dragon")
+  --   end,
+  -- },
+  -- {
+  --   "rose-pine/neovim",
+  --   config = function()
+  --     vim.cmd.colorscheme("rose-pine-moon")
+  --   end,
+  -- },
+  -- {
+  --   "navarasu/onedark.nvim",
+  --   config = function()
+  --     require("onedark").setup({
+  --       style = "darker",
+  --     })
+  --     vim.cmd.colorscheme("onedark")
+  --   end,
+  -- },
+}
