a06989f4cb8add3f87f21d7068ba0be1bce79836
Maciej Gębarski <mgebarski@gmail.com>
2024-04-29 21:25:45 +0200

    Change diagnostic icons and and to neo-tree

--- lua/plugins/neo_tree.lua
+++ lua/plugins/neo_tree.lua
@@ -7,6 +7,10 @@ return {
     "MunifTanjim/nui.nvim",
   },
   config = function()
+    vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
+    vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
+    vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
+    vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
     require("neo-tree").setup({
       auto_clean_after_session_restore = true,
       close_if_last_window = true,
@@ -15,16 +19,16 @@ return {
         winbar = true,
         content_layout = "center",
         sources = {
-          { source = "filesystem", display_name = "File" },
-          { source = "buffers", display_name = "Bufs" },
-          { source = "git_status", display_name = "Git" },
+          { source = "filesystem",  display_name = "File" },
+          { source = "buffers",     display_name = "Bufs" },
+          { source = "git_status",  display_name = "Git" },
           { source = "diagnostics", display_name = "Diagnostic" },
         },
       },
       filesystem = {
         follow_current_file = { enabled = true },
         hijack_netrw_behavior = "open_current",
-        use_libuv_file_watcher = vim.fn.has "win32" ~= 1,
+        use_libuv_file_watcher = vim.fn.has("win32") ~= 1,
       },
       commands = {
         system_open = function(state)
@@ -44,7 +48,7 @@ return {
           if node.type == "directory" or node:has_children() then
             if not node:is_expanded() then -- if unexpanded, expand
               state.commands.toggle_node(state)
-            else -- if expanded and has children, seleect the next child
+            else                     -- if expanded and has children, seleect the next child
               require("neo-tree.ui.renderer").focus_node(state, node:get_child_ids()[1])
             end
           else -- if not a directory just open it
@@ -67,7 +71,9 @@ return {
             ["URI"] = vim.uri_from_fname(filepath),
           }
 
-          local options = vim.tbl_filter(function(val) return vals[val] ~= "" end, vim.tbl_keys(vals))
+          local options = vim.tbl_filter(function(val)
+            return vals[val] ~= ""
+          end, vim.tbl_keys(vals))
           if vim.tbl_isempty(options) then
             -- utils.notify("No values to copy", vim.log.levels.WARN)
             return
@@ -75,7 +81,9 @@ return {
           table.sort(options)
           vim.ui.select(options, {
             prompt = "Choose to copy to clipboard:",
-            format_item = function(item) return ("%s: %s"):format(item, vals[item]) end,
+            format_item = function(item)
+              return ("%s: %s"):format(item, vals[item])
+            end,
           }, function(choice)
             local result = vals[choice]
             if result then
@@ -87,9 +95,9 @@ return {
         find_in_dir = function(state)
           local node = state.tree:get_node()
           local path = node:get_id()
-          require("telescope.builtin").find_files {
+          require("telescope.builtin").find_files({
             cwd = node.type == "directory" and path or vim.fn.fnamemodify(path, ":h"),
-          }
+          })
         end,
       },
       window = {
@@ -104,26 +112,16 @@ return {
           l = "child_or_open",
           o = "open",
         },
-      }
+      },
     })
     local f = function()
       if vim.bo.filetype == "neo-tree" then
-        vim.cmd.wincmd "p"
+        vim.cmd.wincmd("p")
       else
-        vim.cmd.Neotree "focus"
+        vim.cmd.Neotree("focus")
       end
     end
-    vim.keymap.set(
-      'n',
-      '<leader>e',
-      ':Neotree toggle<CR>',
-      {}
-    )
-    vim.keymap.set(
-      'n',
-      '<leader>o',
-      f,
-      {}
-    )
-  end
+    vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>", {})
+    vim.keymap.set("n", "<leader>o", f, {})
+  end,
 }
