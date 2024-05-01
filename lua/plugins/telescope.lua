7fd790a40f73a310f989b93ba8b72bd1329111e1
Maciej Gębarski <mgebarski@gmail.com>
2024-05-01 16:20:39 +0200

    Telescope add ctrl jk keymaps, add smart path etc

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,13 +5,29 @@ return {
 		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
 			local builtin = require("telescope.builtin")
+			local actions = require("telescope.actions")
+			require("telescope").setup({
+				defaults = {
+					prompt_prefix = " ",
+					selection_caret = " ",
+					path_display = { "smart" },
+					mappings = {
+						i = {
+							["<C-n>"] = actions.cycle_history_next,
+							["<C-p>"] = actions.cycle_history_prev,
+							["<C-j>"] = actions.move_selection_next,
+							["<C-k>"] = actions.move_selection_previous,
+						},
+						n = { q = actions.close },
+					},
+				},
+			})
 			vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 			vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 			vim.keymap.set("n", "<leader>fw", builtin.live_grep, {})
 			vim.keymap.set("n", "<leader>fo", builtin.oldfiles, {})
 			vim.keymap.set("n", "<leader>ft", builtin.help_tags, {})
 			vim.keymap.set("n", "<leader>fe", builtin.diagnostics, {})
-			-- vim.keymap.set('n', '<leader>fd', '<cmd>Telescope diagnostics<CR>', {})
 		end,
 	},
 	{
