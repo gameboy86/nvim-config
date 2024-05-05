72d795a83e066338fe01c5ede492f9d5fc62f54d
Maciej Gębarski <mgebarski@gmail.com>
2024-05-05 20:13:41 +0200

    Add formatter for yaml and json

--- lua/plugins/none_ls.lua
+++ lua/plugins/none_ls.lua
@@ -5,29 +5,30 @@ return {
 		local null_ls = require("null-ls")
 		null_ls.setup({
 			sources = {
-				null_ls.builtins.formatting.stylua,
-				-- null_ls.builtins.formatting.jq,
-				null_ls.builtins.formatting.gofmt,
 				null_ls.builtins.code_actions.gomodifytags,
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.gofmt,
 				null_ls.builtins.formatting.golines,
 				null_ls.builtins.formatting.goimports,
-				-- null_ls.builtins.diagnostics.vulture,
 				null_ls.builtins.formatting.black,
 				null_ls.builtins.formatting.isort,
+				null_ls.builtins.formatting.yamlfix,
+				null_ls.builtins.formatting.yamlfmt,
+				null_ls.builtins.formatting.prettier,
 			},
-      on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                group = augroup,
-                buffer = bufnr,
-                callback = function()
-                    -- vim.lsp.buf.formatting_sync()
-                  vim.lsp.buf.format({ async = false })
-                end,
-            })
-        end
-    end,
+			on_attach = function(client, bufnr)
+				if client.supports_method("textDocument/formatting") then
+					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						group = augroup,
+						buffer = bufnr,
+						callback = function()
+							-- vim.lsp.buf.formatting_sync()
+							vim.lsp.buf.format({ async = false })
+						end,
+					})
+				end
+			end,
 		})
 		vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format, {})
 	end,
