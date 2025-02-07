72d26587052c731ad6eeddd1f375cb12331fe623
Maciej Gębarski <mgebarski@gmail.com>
2025-02-07 11:45:12 +0100

    Config noice.lua

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,15 +1,35 @@
 return {
 	"folke/noice.nvim",
 	event = "VeryLazy",
-	opts = {
-		-- add any options here
-	},
+	opts = {},
 	dependencies = {
-		-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
 		"MunifTanjim/nui.nvim",
-		-- OPTIONAL:
-		--   `nvim-notify` is only needed, if you want to use the notification view.
-		--   If not available, we use `mini` as the fallback
 		"rcarriga/nvim-notify",
-	}
+	},
+	lsp = {
+		hover = {
+			enabled = true,
+		},
+		signature = {
+			enabled = true,
+		},
+		override = {
+			['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+			['vim.lsp.util.stylize_markdown'] = true,
+			['cmp.entry.get_documentation'] = true,
+		},
+	},
+	routes = {
+		{
+			filter = {
+				event = "lsp",
+				kind = "progress",
+				cond = function(message)
+					local client = vim.tbl_get(message.opts, "progress", "client")
+					return client == "null-ls"
+				end,
+			},
+			opts = { skip = true },
+		},
+	},
 }
