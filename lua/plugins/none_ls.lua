9b5e1f2699bf9646f93509298bda4ddbd9d38c97
Maciej Gębarski <mgebarski@gmail.com>
2025-02-24 16:33:02 +0100

    fix: none_ls prettier

--- lua/plugins/none_ls.lua
+++ lua/plugins/none_ls.lua
@@ -6,16 +6,16 @@ return {
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.code_actions.gomodifytags,
-				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.gofmt,
 				null_ls.builtins.formatting.golines,
 				null_ls.builtins.formatting.goimports,
 				null_ls.builtins.formatting.black,
 				null_ls.builtins.formatting.isort,
-				-- null_ls.builtins.formatting.yamlfix,
-				-- null_ls.builtins.formatting.yamlfmt,
 				null_ls.builtins.formatting.prettier,
 				null_ls.builtins.diagnostics.golangci_lint,
+				null_ls.builtins.formatting.stylua,
+				-- null_ls.builtins.formatting.yamlfix,
+				-- null_ls.builtins.formatting.yamlfmt,
 			},
 			on_attach = function(client, bufnr)
 				local format = function()
