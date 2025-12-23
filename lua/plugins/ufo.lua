a1f805eb3d43f986faf51a2e35ccc7bfb13a94b4
gameboy <mgebarski@gmail.com>
2025-12-23 12:49:16 +0100

    fix: ufo load

--- lua/plugins/ufo.lua
+++ lua/plugins/ufo.lua
@@ -1,8 +1,11 @@
 return {
 	"kevinhwang91/nvim-ufo",
-	event = "BufReadPre",
+	lazy = false, -- ✅ najważniejsze
 	dependencies = { "kevinhwang91/promise-async" },
 	opts = function()
 		return require("config.ufo").opts()
+	end,
+	config = function(_, opts)
+		require("ufo").setup(opts)
 	end,
 }
