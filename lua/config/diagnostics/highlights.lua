df14bc93a6903c87b95d62746e78c8276832284d
Maciej Gębarski <mgebarski@gmail.com>
2025-12-27 16:04:01 +0100

    feat: add higlights for msg area

--- lua/config/diagnostics/highlights.lua
+++ lua/config/diagnostics/highlights.lua
@@ -1,5 +1,18 @@
 local M = {}
 
-function M.setup() end
+local function set_msg_area_hl()
+	vim.api.nvim_set_hl(0, "MsgArea", { bg = "#2b2140" })
+	vim.api.nvim_set_hl(0, "MsgSeparator", { bg = "#2b2140" })
+	vim.api.nvim_set_hl(0, "ModeMsg", { bg = "#2b2140" })
+	vim.api.nvim_set_hl(0, "Cmdline", { bg = "#2b2140" })
+end
+
+function M.setup()
+	set_msg_area_hl()
+
+	vim.api.nvim_create_autocmd("ColorScheme", {
+		callback = set_msg_area_hl,
+	})
+end
 
 return M
