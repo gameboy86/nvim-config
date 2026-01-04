a313d0c669ed65fabe8eb55b36e3c9b46970254d
Maciej Gębarski <mgebarski@gmail.com>
2026-01-04 01:10:15 +0100

    feat: add diable confor formating

--- lua/config/conform.lua
+++ lua/config/conform.lua
@@ -1,47 +1,54 @@
--- config/conform.lua
--- Conform.nvim configuration module.
---
--- This file returns only options (opts) used by the Conform plugin.
--- Conform is a formatter runner: it formats buffers by calling external formatters
--- (like gofmt, black, stylua, prettier) based on the current filetype.
-
 local M = {}
 
 function M.opts()
 	return {
-		-- Automatically format the buffer when you save the file.
-		-- timeout_ms: maximum time Conform will wait for formatters to finish.
-		-- lsp_fallback: if true, Conform will use LSP formatting when no formatter is configured.
-		--              here it's false -> if there is no configured formatter, Conform won't format via LSP.
-		format_on_save = {
-			timeout_ms = 2000,
-			lsp_fallback = false,
-		},
+		format_on_save = false,
 
-		-- Map filetypes to a list of formatters to run.
-		-- The order matters: Conform will run them in sequence.
 		formatters_by_ft = {
-			-- Go: goimports (imports + formatting), gofmt (standard formatter), golines (wrap long lines)
 			go = { "goimports", "gofmt", "golines" },
-
-			-- Python: isort (import sorting), black (code formatting)
 			python = { "isort", "black" },
-
-			-- JSON/JSONC: prettier (consistent formatting, supports comments in jsonc)
 			json = { "prettier" },
 			jsonc = { "prettier" },
-
-			-- YAML: yamlfmt (YAML formatting)
 			yaml = { "yamlfmt" },
 			yml = { "yamlfmt" },
-
-			-- XML: xmllint (format XML)
 			xml = { "xmllint" },
-
-			-- Lua: stylua (Lua formatting)
 			lua = { "stylua" },
 		},
 	}
+end
+
+function M.init()
+	local conform = require("conform")
+	local group = vim.api.nvim_create_augroup("FormatOnSave", { clear = true })
+
+	vim.api.nvim_create_autocmd("BufWritePre", {
+		group = group,
+		callback = function(args)
+			if vim.g.disable_autoformat or vim.b[args.buf].disable_autoformat then
+				return
+			end
+
+			conform.format({
+				bufnr = args.buf,
+				timeout_ms = 2000,
+				lsp_format = "never",
+			})
+		end,
+	})
+
+	vim.api.nvim_create_user_command("FormatDisable", function(opts)
+		if opts.bang then
+			-- tylko bieżący buffer
+			vim.b.disable_autoformat = true
+		else
+			vim.g.disable_autoformat = true
+		end
+	end, { bang = true, desc = "Disable autoformat-on-save" })
+
+	vim.api.nvim_create_user_command("FormatEnable", function()
+		vim.b.disable_autoformat = false
+		vim.g.disable_autoformat = false
+	end, { desc = "Enable autoformat-on-save" })
 end
 
 return M
--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -4,4 +4,8 @@ return {
 	opts = function()
 		return require("config.conform").opts()
 	end,
+	config = function(_, opts)
+		require("conform").setup(opts)
+		require("config.conform").init()
+	end,
 }
